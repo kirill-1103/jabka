77aec47ba4f9ab8bc672af0fc0e1b261dcac21fe
Therenegadee <melkindaniil@yandex.ru>
2023-10-17 22:04:59 +0300

    Fixed OpenAPI schema

--- study-service/src/main/java/sovcombank/jabka/studyservice/controllers/HomeworkController.java
+++ study-service/src/main/java/sovcombank/jabka/studyservice/controllers/HomeworkController.java
@@ -1,8 +1,9 @@
 package sovcombank.jabka.studyservice.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.sovcombank.openapi.api.HomeworkApiDelegate;
 import ru.sovcombank.openapi.model.HomeworkOpenAPI;
 
@@ -11,38 +12,52 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/study/homework")
 public class HomeworkController implements HomeworkApiDelegate {
+
+    @PostMapping
     @Override
-    public ResponseEntity<Void> createHomework(HomeworkOpenAPI homeworkOpenAPI) {
-        return HomeworkApiDelegate.super.createHomework(homeworkOpenAPI);
+    public ResponseEntity<Void> createHomework(HomeworkOpenAPI homework, List<MultipartFile> file) {
+        return HomeworkApiDelegate.super.createHomework(homework, file);
     }
 
+    @DeleteMapping
     @Override
     public ResponseEntity<Void> deleteHomework(Long id) {
         return HomeworkApiDelegate.super.deleteHomework(id);
     }
 
+    @GetMapping
     @Override
     public ResponseEntity<List<HomeworkOpenAPI>> getAllHomeworks() {
         return HomeworkApiDelegate.super.getAllHomeworks();
     }
 
+    @GetMapping("/{id}")
     @Override
-    public ResponseEntity<HomeworkOpenAPI> getHomeworkById(Long id) {
+    public ResponseEntity<HomeworkOpenAPI> getHomeworkById(
+            @Valid @PathVariable(name = "id") Long id
+    ) {
         return HomeworkApiDelegate.super.getHomeworkById(id);
     }
 
+    @GetMapping("/materials/{materialsId}")
     @Override
-    public ResponseEntity<List<HomeworkOpenAPI>> getHomeworkByMaterialsId(Long materialsId) {
+    public ResponseEntity<List<HomeworkOpenAPI>> getHomeworkByMaterialsId(
+            @Valid @PathVariable(name = "materialsId") Long materialsId
+    ) {
         return HomeworkApiDelegate.super.getHomeworkByMaterialsId(materialsId);
     }
 
+    @GetMapping("/student/{studentId}")
     @Override
-    public ResponseEntity<List<HomeworkOpenAPI>> getHomeworkByStudentId(Long studentId) {
+    public ResponseEntity<List<HomeworkOpenAPI>> getHomeworkByStudentId(
+            @Valid @PathVariable(name = "studentId")Long studentId
+    ) {
         return HomeworkApiDelegate.super.getHomeworkByStudentId(studentId);
     }
 
+    @PutMapping
     @Override
-    public ResponseEntity<Void> updateHomework(HomeworkOpenAPI homeworkOpenAPI) {
-        return HomeworkApiDelegate.super.updateHomework(homeworkOpenAPI);
+    public ResponseEntity<Void> updateHomework(HomeworkOpenAPI homework, List<MultipartFile> file) {
+        return HomeworkApiDelegate.super.updateHomework(homework, file);
     }
 }
