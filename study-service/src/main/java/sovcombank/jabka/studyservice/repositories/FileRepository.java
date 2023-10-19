252c935de81db4e4cc5ab0ec1713b6c0c03e4f8c
Therenegadee <melkindaniil@yandex.ru>
2023-10-19 09:31:26 +0300

    changes in openapi

---
+++ study-service/src/main/java/sovcombank/jabka/studyservice/repositories/FileRepository.java
@@ -0,0 +1,10 @@
+package sovcombank.jabka.studyservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sovcombank.jabka.studyservice.models.File;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+
+}
