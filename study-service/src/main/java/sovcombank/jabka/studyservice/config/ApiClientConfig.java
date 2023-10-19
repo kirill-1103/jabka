2564d8422379211aef792998cb0a650bc3506672
Therenegadee <melkindaniil@yandex.ru>
2023-10-19 18:23:31 +0300

    user open api

--- study-service/src/main/java/sovcombank/jabka/studyservice/config/ApiClientConfig.java
+++ study-service/src/main/java/sovcombank/jabka/studyservice/config/ApiClientConfig.java
@@ -1,32 +1,22 @@
 package sovcombank.jabka.studyservice.config;
 
 import lombok.RequiredArgsConstructor;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.RestTemplate;
 import ru.sovcombank.openapi.user.ApiClient;
-import ru.sovcombank.openapi.user.ApiException;
 import ru.sovcombank.openapi.user.api.AuthorizationApi;
 import ru.sovcombank.openapi.user.api.UserApi;
-import ru.sovcombank.openapi.user.model.JwtResponseOpenApi;
-import ru.sovcombank.openapi.user.model.LoginRequestOpenApi;
 import sovcombank.jabka.libs.security.state.TokenKeeper;
 
-import java.io.IOException;
 import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApiClientConfig {
 
-    @Value("${user-service.base-url}")
-    private String baseUserServiceUrl;
+//    @Value("${user-service.base-url}")
+//    private String baseUserServiceUrl;
 
     @Value("${spring.application.api-name}")
     private String accessApiName;
@@ -37,7 +27,7 @@ public class ApiClientConfig {
     @Bean
     public ApiClient userClient() {
         ApiClient apiClient = ru.sovcombank.openapi.user.Configuration.getDefaultApiClient();
-        apiClient.updateBaseUri(baseUserServiceUrl);
+//        apiClient.updateBaseUri(baseUserServiceUrl);
 
 
         apiClient.setRequestInterceptor((http) -> {
