58da55684de6a3d6945f16bc2845f0cd4beec5a0
Therenegadee <melkindaniil@yandex.ru>
2023-10-21 21:38:11 +0300

    fixed db error with user

--- user-service/src/main/java/sovcombank/jabka/userservice/model/Role.java
+++ user-service/src/main/java/sovcombank/jabka/userservice/model/Role.java
@@ -8,13 +8,16 @@ import lombok.Setter;
 import ru.sovcombank.openapi.model.ERoleOpenApi;
 
 @Entity
+@Table(name = "roles")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Role {
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "roles_seq_generator")
+    @SequenceGenerator(name = "roles_seq_generator", sequenceName = "roles_seq", allocationSize = 1)
     private Long id;
 
     @Enumerated(EnumType.STRING)
