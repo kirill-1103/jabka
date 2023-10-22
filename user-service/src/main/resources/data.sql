e1cac6acdb999e2f58275f5ab8da10399b850cf4
Therenegadee <melkindaniil@yandex.ru>
2023-10-22 18:02:00 +0300

    fixes in controllers

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -14,14 +14,18 @@ ON CONFLICT DO NOTHING;
 insert into users(id,login,name,surname,email,password)
 values
     (DEFAULT,'API_ADMIN','API_ADMIN','API_ADMIN','API_ADMIN@admin.admin','$2a$10$WpQf5Ph9ByyQRa4dyvLZS.3hIzjxkb1TbWd4aSLt7rsMtyZdoLoKq'),
-    (DEFAULT, 'STUDENT_1', 'STUDENT_1', 'STUDENT_1', 'STUDENT_1@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO'),
-    (DEFAULT, 'STUDENT_2', 'STUDENT_2', 'STUDENT_2', 'STUDENT_2@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO'),
-    (DEFAULT, 'STUDENT_3', 'STUDENT_3', 'STUDENT_3', 'STUDENT_3@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO'),
-    (DEFAULT, 'STUDENT_4', 'STUDENT_4', 'STUDENT_4', 'STUDENT_4@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO'),
-    (DEFAULT, 'STUDENT_5', 'STUDENT_5', 'STUDENT_5', 'STUDENT_5@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO'),
     (DEFAULT, 'TEACHER_1', 'TEACHER_1', 'TEACHER_1', 'TEACHER_1@teacher.teacher', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO'),
     (DEFAULT, 'TEACHER_2', 'TEACHER_2', 'TEACHER_2', 'TEACHER_2@teacher.teacher', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO'),
     (DEFAULT, 'TEACHER_3', 'TEACHER_3', 'TEACHER_3', 'TEACHER_3@teacher.teacher', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO')
+ON CONFLICT DO NOTHING;
+
+insert into users(id,login,name,surname,email,password,group_number)
+values
+    (DEFAULT, 'STUDENT_1', 'STUDENT_1', 'STUDENT_1', 'STUDENT_1@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO', 'Group A'),
+    (DEFAULT, 'STUDENT_2', 'STUDENT_2', 'STUDENT_2', 'STUDENT_2@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO', 'Group A'),
+    (DEFAULT, 'STUDENT_3', 'STUDENT_3', 'STUDENT_3', 'STUDENT_3@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO', 'Group B'),
+    (DEFAULT, 'STUDENT_4', 'STUDENT_4', 'STUDENT_4', 'STUDENT_4@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO', 'Group A'),
+    (DEFAULT, 'STUDENT_5', 'STUDENT_5', 'STUDENT_5', 'STUDENT_5@student.student', '$2a$12$LmxAlgfG9z/cuvLo8ZohHOjHNrSI8h5CjiZ8sZ4wKHuTihxCcKwcO', 'Group C')
 ON CONFLICT DO NOTHING;
 
 insert into user_roles (user_id, role_id)
