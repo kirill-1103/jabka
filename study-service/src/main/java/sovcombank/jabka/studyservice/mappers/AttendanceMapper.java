0d45422f2a256fbd6b386b6d9f4b7d08bac44f5c
kirill kreysmann <kreysmann.kirya@mail.ru>
2023-10-22 15:14:40 +0300

    fix

--- study-service/src/main/java/sovcombank/jabka/studyservice/mappers/AttendanceMapper.java
+++ study-service/src/main/java/sovcombank/jabka/studyservice/mappers/AttendanceMapper.java
@@ -7,17 +7,19 @@ import sovcombank.jabka.studyservice.models.AttendanceStatistics;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = SubjectMapper.class)
+@Mapper(componentModel = "spring", uses = {ScheduleMapper.class})
 public interface AttendanceMapper {
-//    @Mapping(target = "schedule", source = "schedule")
+    @Mapping(target = "status", source = "attendanceStatus")
     AttendanceStatisticsOpenApi toOpenApi(AttendanceStatistics attendanceStatistics);
 
-//    @Mapping(target = "schedule", source = "schedule")
+
+    @Mapping(target = "attendanceStatus", source = "status")
     AttendanceStatistics toAttendanceStatistics(AttendanceStatisticsOpenApi attendanceStatisticsOpenApi);
 
-//    @Mapping(target = "schedule", source = "schedule")
+    @Mapping(target = "status", source = "attendanceStatus")
     List<AttendanceStatisticsOpenApi> toOpenApiList(List<AttendanceStatistics> list);
 
-//    @Mapping(target = "schedule", source = "schedule")
+
+    @Mapping(target = "attendanceStatus", source = "status")
     List<AttendanceStatistics> toListAttendanceStatistics(List<AttendanceStatisticsOpenApi> list);
 }
