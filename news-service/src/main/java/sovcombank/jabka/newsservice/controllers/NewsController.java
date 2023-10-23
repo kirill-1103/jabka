76f1920e2a0bfc318b80bedd9a2cac4ba51cf434
Therenegadee <melkindaniil@yandex.ru>
2023-10-23 17:10:43 +0300

    role checking in news service

--- news-service/src/main/java/sovcombank/jabka/newsservice/controllers/NewsController.java
+++ news-service/src/main/java/sovcombank/jabka/newsservice/controllers/NewsController.java
@@ -21,24 +21,29 @@ public class NewsController implements NewsApiDelegate {
 
     @Override
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<Void> createNewsPost(@RequestBody NewsOpenAPI newsOpenAPI) {
         return newsService.createNewsPost(newsOpenAPI);
     }
 
     @Override
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT') or hasRole('MODERATOR') or hasRole('TEACHER') " +
+            "or hasRole('CURATOR') or hasRole('COMMITTE') or hasRole('ENROLLEE')")
     public ResponseEntity<List<NewsOpenAPI>> showAllNewsInfo() {
         return newsService.showAllNewsInfo();
     }
 
     @Override
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<Void> deleteNewsById(@Valid @PathVariable Long id) {
         return newsService.deleteNewsById(id);
     }
 
     @Override
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<Void> editNewsById(@PathVariable(name = "id") Long id,
                                              @Valid @RequestBody NewsOpenAPI newsOpenAPI) {
         return newsService.editNewsById(id, newsOpenAPI);
@@ -46,6 +51,8 @@ public class NewsController implements NewsApiDelegate {
 
     @Override
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT') or hasRole('MODERATOR') or hasRole('TEACHER') " +
+            "or hasRole('CURATOR') or hasRole('COMMITTE') or hasRole('ENROLLEE')")
     public ResponseEntity<NewsOpenAPI> showNewsIdInfo(@Valid @PathVariable("id") Long id) {
         return newsService.showNewsIdInfo(id);
     }
--- news-service/src/main/java/sovcombank/jabka/newsservice/utils/PathUtilsImpl.java
+++ news-service/src/main/java/sovcombank/jabka/newsservice/utils/PathUtilsImpl.java
@@ -12,6 +12,7 @@ public class PathUtilsImpl implements PathUtils {
     };
 
     private final static String[] AUTHORIZED_PATHS = new String[]{
+            "/api/news/**"
     };
 
     @Override
@@ -26,36 +27,36 @@ public class PathUtilsImpl implements PathUtils {
 
     @Override
     public @NonNull String[] getForAdmin() {
-        return new String[0];
+        return AUTHORIZED_PATHS;
     }
 
     @Override
     public @NonNull String[] getForStudent() {
-        return new String[0];
+        return AUTHORIZED_PATHS;
     }
 
     @Override
     public @NonNull String[] getForTeacher() {
-        return new String[0];
+        return AUTHORIZED_PATHS;
     }
 
     @Override
     public @NonNull String[] getForCurator() {
-        return new String[0];
+        return AUTHORIZED_PATHS;
     }
 
     @Override
     public @NonNull String[] getForEnrollee() {
-        return new String[0];
+        return AUTHORIZED_PATHS;
     }
 
     @Override
     public @NonNull String[] getForCommitte() {
-        return new String[0];
+        return AUTHORIZED_PATHS;
     }
 
     @Override
     public @NonNull String[] getForModerator() {
-        return new String[0];
+        return AUTHORIZED_PATHS;
     }
 }
