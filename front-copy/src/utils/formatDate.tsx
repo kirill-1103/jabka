1c71eeb56f6f002f8e329aa170581ad66eb8e3de
kirill kreysmann <kreysmann.kirya@mail.ru>
2023-10-23 19:56:47 +0300

    rename

--- front-copy/src/utils/formatDate.tsx
+++
@@ -1,5 +0,0 @@
-export function formatDate(dateString: string) {
-  const options :Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  const date = new Date(dateString);
-  return date.toLocaleDateString('ru-RU', options);
-}
