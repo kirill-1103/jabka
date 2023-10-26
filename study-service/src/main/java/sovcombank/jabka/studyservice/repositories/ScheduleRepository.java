98ed95eec7b88a89364eae168a20349775f84ee4
kirill kreysmann <kreysmann.kirya@mail.ru>
2023-10-26 18:55:51 +0300

    fix

--- study-service/src/main/java/sovcombank/jabka/studyservice/repositories/ScheduleRepository.java
+++ study-service/src/main/java/sovcombank/jabka/studyservice/repositories/ScheduleRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findByProfessorProfessorId(Long id);
+
+    List<Schedule> findByGroupId(Long id);
 }
